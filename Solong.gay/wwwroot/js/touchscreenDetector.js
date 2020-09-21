64e56d222fed81624d585969ccd2a945d1f2eb2d
Thomas Campbell-Adams <thcampb@microsoft.com>
2020-09-21 12:39:05 -0700

    Updating to work on iOS/Android

---
+++ Solong.gay/wwwroot/js/touchscreenDetector.js
@@ -0,0 +1,9 @@
+﻿var TouchScreenDetect = (function () {
+    return {
+        Detect: function () {
+            var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+            return /windows phone/i.test(userAgent) || /android/i.test(userAgent) || /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+        }
+    }
+})();
